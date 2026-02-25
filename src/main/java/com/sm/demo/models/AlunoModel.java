f53f947e64dfbcff7bba23fc67c53ee458b2948d
cayque miranda <cayquemiranda2020@gmail.com>
2026-02-24 21:40:04 -0300

    model criada

---
+++ src/main/java/com/sm/demo/models/AlunoModel.java
@@ -0,0 +1,52 @@
+package com.sm.demo.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TBL_ALUNO")
+public class AlunoModel {
+
+    @Id // CRIA A CHAVE PRIMARIA NO BANCO
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String matricula;
+    private String endereço;
+
+    public AlunoModel() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+}
